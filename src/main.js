e85efc2eff2d5d97716efc16213a143b43aed566
Victor Ozoterio <victorozoterio@gmail.com>
2025-02-28 14:51:42 -0300

    feat: definindo horários de atendimento

--- src/main.js
+++ src/main.js
@@ -7,6 +7,3 @@ import "./libs/dayjs.js";
 import "./styles/global.css";
 import "./styles/form.css";
 import "./styles/schedule.css";
-
-import dayjs from "dayjs";
-console.log(dayjs().format("DD/MM - HH:mm"));
