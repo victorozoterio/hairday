c655122f38a971358eed003bad9a01a2d3bb6a92
Victor Ozoterio <victorozoterio@gmail.com>
2025-02-28 10:43:25 -0300

    feat: incluindo o css na compilação

---
+++ src/main.js
@@ -0,0 +1,6 @@
+"use strict"; // aplica o modo estrito e nos ajuda a manter boas práticas de escrita no javascript
+
+// CSS
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/schedule.css";
