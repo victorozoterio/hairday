278b937a42f173c7b85bf6d0ebb5792d85548190
Victor Ozoterio <victorozoterio@gmail.com>
2025-02-28 12:05:59 -0300

    feat: instalando e configurando o dayjs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 "use strict"; // aplica o modo estrito e nos ajuda a manter boas práticas de escrita no javascript
 
+// Configuração do dayjs
+import "./libs/dayjs.js";
+
 // CSS
 import "./styles/global.css";
 import "./styles/form.css";
 import "./styles/schedule.css";
+
+import dayjs from "dayjs";
+console.log(dayjs().format("DD/MM - HH:mm"));
