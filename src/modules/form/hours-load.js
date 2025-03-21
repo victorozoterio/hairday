181b71dbe57426ac025bfd58e9d697f9d6708b6c
Victor Ozoterio <victorozoterio@gmail.com>
2025-03-20 23:37:26 -0300

    feat: separando os horários pelo período

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -15,13 +15,26 @@ export function hoursLoad({ date }) {
 	});
 
 	// Renderiza os horários
-	opening.forEach(({hour, available}) => {
+	opening.forEach(({ hour, available }) => {
 		const li = document.createElement("li");
 
 		li.classList.add("hour");
 		li.classList.add(available ? "hour-available" : "hour-unavailable");
 
 		li.textContent = hour;
+
+		if (hour === "9:00") hourHeaderAdd("Manhã");
+		if (hour === "13:00") hourHeaderAdd("Tarde");
+		if (hour === "18:00") hourHeaderAdd("Noite");
+
 		hours.append(li);
-	})
+	});
+}
+
+function hourHeaderAdd(title) {
+	const header = document.createElement("li");
+	header.classList.add("hour-period");
+	header.textContent = title;
+
+	hours.append(header);
 }
