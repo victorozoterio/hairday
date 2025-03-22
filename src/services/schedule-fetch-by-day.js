ad1ee757ad325dd4fea2b75908fba8eb6ea4d130
Victor Ozoterio <victorozoterio@gmail.com>
2025-03-22 00:33:08 -0300

    feat: serviço para buscar agendamentos do dia

---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,19 @@
+import dayjs from "dayjs";
+import { apiConfig } from "./api-config";
+
+export async function scheduleFetchByDay({ date }) {
+    try {
+        // Faz a requisição.
+        const response = await fetch(`${apiConfig.baseUrl}/schedules`)
+
+        // Converter para JSON.
+        const data = await response.json();
+
+        // Filtra os agendamentos pelo dia selecionado.
+        const dailySchedules = data.filter((schedule) => dayjs(date).isSame(schedule.when, "day"));
+        return dailySchedules;
+    } catch (error) {
+        console.log(error);
+        alert('Não foi possível buscar os agendamentos do dia selecionado. Tente novamente mais tarde.')
+    }
+}
