3a9c30cd695e98c72c56d1211d8dbcea67134f77
Victor Ozoterio <victorozoterio@gmail.com>
2025-03-22 00:39:14 -0300

    feat: buscando os agendamentos na API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,16 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
 
 // Seleciona o input de data.
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
 	// Obtém a data do input.
 	const date = selectedDate.value;
+
+	// Busca na API os agendamentos do dia.
+	const schedules = await scheduleFetchByDay({ date });
+	console.log(schedules)
 
 	// Renderiza as horas disponíveis.
 	hoursLoad({ date });
