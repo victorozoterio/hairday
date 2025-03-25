b6d7821e4e864d3717396d0d175d7d821a74cd00
Victor Ozoterio <victorozoterio@gmail.com>
2025-03-25 17:40:36 -0300

    feat: atualizando a lista após realizar um agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
-import { scheduleNew } from "../../services/schedule-new";
+import { scheduleNew } from "../../services/schedule-new.js";
+import { schedulesDay } from "../schedules/load.js";
 
 const form = document.querySelector("form");
 const clientName = document.getElementById("client");
@@ -33,7 +34,14 @@ form.onsubmit = async (event) => {
 		// Gera um ID
 		const id = new Date().getTime();
 
+		// Faz o agendamento
 		await scheduleNew({ id, name, when });
+
+		// Recarrega os agendamentos
+		await schedulesDay();
+
+		// Limpa o input de nome do cliente
+		clientName.value = "";
 	} catch (error) {
 		alert("Não foi possível realizar o agendamento.")
 		console.log(error);
